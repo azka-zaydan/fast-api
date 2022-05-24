55d5d6b64a9d830f86d8d7a7ebe2fe66a52a5bca
azka1415 <rafifzaydan4@gmail.com>
2022-05-24 14:27:28 +0700

    Added more stuff

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import pymysql
+pymysql.install_as_MySQLdb()
+
+sqlalchemy_database_url = 'mysql://root:1234@localhost:6033/fastdb'
+
+engine = create_engine(sqlalchemy_database_url)
+
+sessionlocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base = declarative_base()
+
+def get_db_conn():
+    db = sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,32 +1,19 @@
 import os
-from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Response, status, HTTPException,Depends
 from pydantic import BaseModel
-import mysql.connector
+import models
+from database import engine,get_db_conn
+
+models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
-
-
-def get_db_conn():
-    data = {
-        "host": "localhost",
-        "port": "6033",
-        "user": "root",
-        "password": "1234",
-        "database": "fastdb"
-    }
-    try:
-        db_conn = mysql.connector.connect(**data)
-        return db_conn
-    except:
-        print("Error")
 
 
 class Post(BaseModel):
     title: str  # we can use this instead of key value dictionaries
     content: str
-    published: Optional[bool] = True  # set a default value
-    rating: Optional[int] = 0  # make it optional
+    published: bool = True  # set a default value
 
 
 @app.get('/')
@@ -35,24 +22,20 @@ async def root():
 
 
 @app.get('/posts')
-async def all_posts():
-    conn = get_db_conn()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("select * from posts")
-    result = cursor.fetchall()
-    return {"Posts": result}
+async def all_posts(db: Session = Depends(get_db_conn)):
+    dbq = db.query(models.Post)
+    posts = dbq.all()
+    return {"Posts": posts}
 
 
 @app.get('/posts/{search_id}')
-async def get_post_with_id(search_id: int, response: Response):
+async def get_post_with_id(search_id: int):
     conn = get_db_conn()
     cursor = conn.cursor(dictionary=True)
     cursor.execute(f"select * from posts where id={search_id}")
     result = cursor.fetchone()
     if result is None:
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return f"Post with id: {search_id}, does not exist"
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id: {search_id}, does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {search_id}, does not exist")
     else:
         return {
             "Post": result
@@ -60,47 +43,53 @@ async def get_post_with_id(search_id: int, response: Response):
 
 
 # want title, and content both string
-@app.post('/posts',status_code=status.HTTP_201_CREATED)
-async def post(post: Post):
-    conn = get_db_conn()
-    cursor = conn.cursor(dictionary=True)
-    # to_dict = post.dict() # turn into a dict
-    title = post.title
-    content = post.content
-    cursor.execute(f"INSERT INTO posts (title,content) values ('{title}','{content}')")
-    conn.commit()
-    cursor.execute("SELECT * FROM posts ORDER BY id DESC LIMIT 1;")
-    result = cursor.fetchone()
-    conn.commit()
-    return  {
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+async def post(post: Post,db: Session= Depends(get_db_conn)):
+    # conn = get_db_conn()
+    # cursor = conn.cursor(dictionary=True)
+    # # to_dict = post.dict() # turn into a dict
+    # title = post.title
+    # content = post.content
+    # published = post.published
+    # cursor.execute(f"INSERT INTO posts (title,content,published) values ('{title}','{content}',{published})")
+    # conn.commit()
+    # cursor.execute("SELECT * FROM posts ORDER BY id DESC LIMIT 1;")
+    # result = cursor.fetchone()
+    # conn.commit()
+    result =models.Post(title=post.title,content=post.content,published=post.published)
+    return {
         "post": result
     }
 
-@app.delete('/posts/{search_id}',status_code=status.HTTP_204_NO_CONTENT)
+
+@app.delete('/posts/{search_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def del_post(search_id: int):
     conn = get_db_conn()
     cursor = conn.cursor(dictionary=True)
     cursor.execute(f'select * from posts where id={search_id}')
     result = cursor.fetchone()
     if result is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id: {search_id}, does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {search_id}, does not exist")
     else:
         cursor.execute(f"delete from posts where id={search_id}")
         conn.commit()
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+
 @app.put('/posts/{search_id}')
-def update_post(search_id: int,post : Post):
+def update_post(search_id: int, post: Post):
     conn = get_db_conn()
     cursor = conn.cursor(dictionary=True)
     cursor.execute(f'select * from posts where id={search_id}')
     result1 = cursor.fetchone()
     if result1 is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id: {search_id}, does not exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {search_id}, does not exist")
     else:
         title = post.title
         content = post.content
-        cursor.execute(f"update posts set title='{title}', content='{content}' where id={search_id};")
+        published = post.published
+        cursor.execute(
+            f"update posts set title='{title}', content='{content}',published={published} where id={search_id};")
         conn.commit()
         cursor.execute(f'select * from posts where id={search_id}')
         result = cursor.fetchone()
